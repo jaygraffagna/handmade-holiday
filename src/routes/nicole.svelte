e01c9412a89f60b7f3bc39b4a9863ed8d01c3a76
jaygraffagna <jaygraffagna@gmail.com>
2020-12-10 21:06:03 -0600

    nicole complete with iowa

--- src/routes/nicole.svelte
+++ src/routes/nicole.svelte
@@ -1,19 +1,63 @@
+<script>
+  import { onMount, tick } from 'svelte';
+  import Iowa from '../components/Iowa.svx'
+
+  let canvas;
+  let context;
+
+  const draw = () => {
+    context = canvas.getContext("2d");
+    const cWidth = canvas.width;
+    const cHeight = canvas.height;    
+
+    context.fillStyle = "black";  
+    context.fillRect(10, 40, 275, 70);
+
+    context.fillStyle = "#fed602";  
+    context.fillRect(20, 45, 255, 60);
+
+    context.fillStyle = "black";  
+    context.fillRect(30, 80, 235, 20);
+
+    context.font = "900 italic 42px Verdana"; 
+    context.fillStyle = "black";
+    context.fillText("IOWA", 75, 78);
+
+    context.font = "900 italic 28px Verdana"; 
+    context.fillStyle = "#fed602";
+    context.fillText("HAWKEYES", 60, 100);
+  }
+
+  onMount(async () => {
+    await tick()
+    draw();
+  });
+</script>
 <style>
-  figure {
-    float: right;
-    margin: -3em -7em 2em 2em;
-    max-width: 400px;
+  canvas {
+    position: absolute;
+    top: 15%;
+    left: 3%;
+    width: 400px;
+    height: 395px;
+  }
+  #wrapper{
+    position: relative;
+    font-size: 10px;
   }
 
-  figure img {
-    border-radius: 8px;
-  }
-
-  @media (max-width: 1020px) {
-    figure {
-      float: none;
-      margin: 0 auto 2em;
-    }
+  @media (max-width: 500px) {
+      canvas {
+        position: absolute;
+        top: 22%;
+        left: 3.5%;
+        width: 275px;
+        height: 275px;
+      }
+      #wrapper{
+        position: relative;
+        font-size: 7px;
+      }
   }
 </style>
 
@@ -21,7 +65,8 @@
   <title>Nicole</title>
 </svelte:head>
 
-<div class="container">
+<div id="wrapper" class="container">
   <h1>Nicole</h1>
-  https://u-stencil.com/wp-content/uploads/2019/03/Tailgater_Iowa_University_IOWA-1.jpg
+  <Iowa/>
+  <canvas bind:this={canvas} />
 </div>
